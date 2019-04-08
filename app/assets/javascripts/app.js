d78a55501233e2502fe830d883cf21a06f32c9d5
Don Smith <dcsmith@gmail.com>
2019-04-07 20:25:41 -0700

    try to remove es6 for heroku

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,10 +5,10 @@ angular.module('gitactivity', [])
 	$scope.activities=[];
         
         $scope.query = function(){
-        let url='/user/'+$scope.user;
+        var url='/user/'+$scope.user;
 	  $http.get(url).then( function(response) {
               $scope.activities.splice(0,$scope.activities.length);
-              for(let i=0;i<response.data.length;i++){
+              for(var i=0;i<response.data.length;i++){
                   $scope.activities.push(response.data[i]);
               }
 
